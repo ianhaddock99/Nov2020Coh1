6d5a77f7f213f48fec0162575f5841b803caa3e4
Ian Haddock <ianhaddock99@gmail.com>
2021-01-07 11:02:47 -0500

    work

--- week08/4-Thursday/labs/app.js
+++ week08/4-Thursday/labs/app.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs')
+
+//sub routes
+app.use(express.static('public'));
+
+app.use(require('./routes/index'));
+app.use(require('./routes/about'));
+
+
+
+app.listen(3000, ()=>{
+    console.log('Server running on port 3000');
+})
