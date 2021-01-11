5d4435f0fef72bc73916051480d199dd205199ca
Ian Haddock <ianhaddock99@gmail.com>
2021-01-11 13:39:17 -0500

    feedback

--- week08/4-Thursday/labs/artspeakers/app.js
+++ week08/4-Thursday/labs/artspeakers/app.js
@@ -10,6 +10,8 @@ app.use(express.static('public'))
 //route references
 app.use(require('./routes/index'))
 app.use(require('./routes/speakers'))
+app.use(require('./routes/feedback'))
+app.use(require('./routes/api'))
 
 
 app.listen(3000, ()=>{
