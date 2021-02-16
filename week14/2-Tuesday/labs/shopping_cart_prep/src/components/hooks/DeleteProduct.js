28ab3ec643797898ab7418c053ca4da44215ef31
Ian Haddock <ianhaddock99@gmail.com>
2021-02-16 14:11:56 -0500

    new

--- week14/2-Tuesday/labs/shopping_cart_prep/src/components/hooks/DeleteProduct.js
+++ week14/2-Tuesday/labs/shopping_cart_prep/src/components/hooks/DeleteProduct.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import {dispatch} from 'react-redux'
-import {deleteProduct} from '../../actions/cartActions'
+import React from "react";
+import { useDispatch } from 'react-redux';
+import {deleteProduct} from '../../actions/cartActions';
 
 const DeleteProduct = ({product}) => {
 
-  const dispatch = useDispatch();
-  return (
-    <>
-      <button onClick={()=>dispatch(DeleteProduct(product))}>X</button>
-    </>
-  )
-}
+    const dispatch = useDispatch();
 
-export default DeleteProduct
+  return <>
+
+    <button onClick={()=>dispatch(deleteProduct(product))} >X</button>
+
+  </>;
+};
+
+export default DeleteProduct;
