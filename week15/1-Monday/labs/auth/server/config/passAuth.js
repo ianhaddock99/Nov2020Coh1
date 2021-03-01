a346cf6585efb5fb507ca6d146261f2548b30e09
Ian Haddock <ianhaddock99@gmail.com>
2021-03-01 10:13:54 -0500

    mon

--- week15/1-Monday/labs/auth/server/config/passAuth.js
+++ week15/1-Monday/labs/auth/server/config/passAuth.js
@@ -1,53 +1,96 @@
+
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const bcrypt = require('bcryptjs');
-const db = require('../models');
-const config = require('../secrets');
 
-// validating a user credentials email password
-//local strategy emil and password validation
+const bcrypt = require('bcryptjs'); //unencrypt
+const db = require('../models'); //access to user model
+const config = require('../secrets'); //gives access to jwt secret
+
+/**
+ * validating a user's credentials
+ * email, password  => local strategy
+ * jwt
+ */
+
+/**
+ * local strategy
+ * email and password validation
+ */
 
 //options to override username field
-let options ={
+let options = {
     usernameField: 'email'
 }
 
-let localLogin = new LocalStrategy(options, (username, password, done)=>{
-    //check to see if email is in our db
+let localLogin = new LocalStrategy(options, async (email, password, done)=>{
     try{
-        let records =  db.user.findAll({where: {email:email}})
-        if(records !== null){
-            //encrypt password and compare to password in db
+        // check to see if email is in our db 
 
+        let records = await db.user.findAll({where: {email: email}}); //array of objects
+
+        if(records !== null){
+            //encrypt password and compare to password in db 
+           
             bcrypt.compare(password, records[0].password, (err, isMatch)=>{
+
                 //check if err object exists
                 if(err){
                     return done(err);
                 }
-                //mismatching password
-                if(!isMatch){
-                    return done(null,false)
+                //mismatch passwords
+                if(!isMatch) {
+                    return done(null, false)
                 }
+
+                //valid user 
+                
                 return done(null, records[0]);
-            })
+            }) 
 
         }
         else{
+            
+            //no email was found, exit with error
             return done(null, false);
         }
-    
     }
-    catch{
+    catch(error){
+        
+        //somethin g in dabatase retreival
+        return done(error)
+    }
+
+});
+
+
+/**
+ * jwt strategy
+ * validating token
+ */
+let jwtOptions ={
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+}
+
+let jwtLogin = new JwtStrategy(jwtOptions, (payload, done)=>{
+    try{
+        let user = await db.user.findByPk(payload.sub)
+        if(user){
+            done(null, user);
+        }
+        else{
+            done(null, false)
+        }
+    }
+    catch(error){
         return done(error)
     }
 });
 
-// let jwtLogin = new JwtStrategy
 
-passport.use(localLogin)
-
-// passport.use(localLogin);
-// passport.use(jwtLogin);
+passport.use(localLogin);
+passport.use(jwtLogin)
 
